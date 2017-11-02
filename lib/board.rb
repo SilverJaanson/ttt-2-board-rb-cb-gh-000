4a4df7da85ca6e2ca693906294ca73787c205068
silver jaanson <silver.jaanson@tptlive.ee>
2017-11-02 17:47:54 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,32 @@
 row = [" ", " ", " ",br, " "," "," ",br," "," "," "]
 
-# Position 1: Left
-row[0]
+Position 1: upLeft
+  row[0]
 
-# Position 2: Middle
+Position 2: upMiddle
 row[1]
 
-# Position 3: Right
+Position 3: upRight
 row[2]
 
-# Move X to the Position 2, Middle
+Position 4: Left
+row[3]
+
+Position 5: Middle
+row[4]
+
+Position 6: Right
+row[5]
+
+Position 7: downLeft
+row[6]
+
+Position 8: downMiddle
+row[7]
+
+Position 9: downRight
+row[8]
+
+Move X to the Position 2, Middle
 row[1] = "X"
 row #=> [" ", "X", " "]
